1c127ea97221add4e8abe5cb64726953416bfaeb
Aymane Boujeddi <aymane655@gmail.com>
2025-10-30 16:11:24 +0100

    SCRUM-9 add(repository) : created the repository interface extending from the JpaRepository that gets all the main methods for communcating with the database

--- src/main/java/com/gestion/stock/repository/FournisseurRepository.java
+++ src/main/java/com/gestion/stock/repository/FournisseurRepository.java
@@ -1,4 +1,11 @@
 package com.gestion.stock.repository;
 
-public interface FournisseurRepository {
+import com.gestion.stock.entity.Fournisseur;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FournisseurRepository extends JpaRepository<Fournisseur, Long> {
+
+
 }
