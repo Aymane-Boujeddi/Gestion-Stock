281b8e53ce3cac5b39213b12e69a7837bfbc4424
Aymane Boujeddi <aymane655@gmail.com>
2025-10-30 12:11:30 +0100

    add(mapper) : added the Fournisseur mapper to convert the dto in the controller to entity when passing it to the service and in convert the entity to dto when returning a response

--- src/main/java/com/gestion/stock/controller/FournisseurController.java
+++ src/main/java/com/gestion/stock/controller/FournisseurController.java
@@ -1,4 +1,35 @@
 package com.gestion.stock.controller;
 
+
+import com.gestion.stock.dto.request.FournisseurCreateDTO;
+import com.gestion.stock.mapper.FournisseurMapper;
+import com.gestion.stock.service.FournisseurService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+
+
+@RestController
+@RequestMapping("/fournisseurs")
 public class FournisseurController {
+
+    @Autowired
+    private FournisseurService fournisseurService;
+
+    @Autowired
+    private FournisseurMapper mapper;
+
+
+
+
+
+    @PostMapping
+    public void createFournisseur(@RequestBody FournisseurCreateDTO fournisseurCreateDTO){
+
+
+    }
+
+
+
+
 }
---
+++ src/main/java/com/gestion/stock/mapper/FournisseurMapper.java
@@ -0,0 +1,18 @@
+package com.gestion.stock.mapper;
+
+
+import com.gestion.stock.dto.request.FournisseurCreateDTO;
+import com.gestion.stock.dto.response.FournisseurResponseDTO;
+import com.gestion.stock.entity.Fournisseur;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface FournisseurMapper {
+
+    Fournisseur toEntity(FournisseurCreateDTO dto);
+
+    FournisseurResponseDTO toResponseDTO(Fournisseur fournisseur);
+
+}
