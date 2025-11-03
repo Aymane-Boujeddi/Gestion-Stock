4686e187911f76296adf19d959c6813651f7d187
Aymane Boujeddi <aymane655@gmail.com>
2025-11-03 11:59:55 +0100

    SCRUM-32 Créer l'entité Commande avec les relations @ManyToMany ajouter une classe associative DetailsCommande plus ses relations avec Produits

---
+++ src/main/java/com/gestion/stock/entity/Commande.java
@@ -0,0 +1,44 @@
+package com.gestion.stock.entity;
+
+
+import com.gestion.stock.enums.StatutCommande;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "commandes")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Commande {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name= "montant_totale")
+    private Double montanTotale;
+
+    @Column(name = "date_commande")
+    private LocalDateTime dateCommnande;
+
+    @Column(name = "statut_commande")
+    @Enumerated(EnumType.STRING)
+    private StatutCommande statutCommande;
+
+
+    @OneToMany(mappedBy = "commande")
+    private List<DetailsCommande> detailsCommandes;
+
+
+
+
+
+
+}
