69ac8a7b44c6cdeb846bc419df8facc3ab68b67a
Aymane Boujeddi <aymane655@gmail.com>
2025-11-05 12:19:41 +0100

    Add updateEntity method to DetailsCommandeMapper for updating DetailsCommande entities

--- src/main/java/com/gestion/stock/mapper/DetailsCommandeMapper.java
+++ src/main/java/com/gestion/stock/mapper/DetailsCommandeMapper.java
@@ -3,15 +3,14 @@ package com.gestion.stock.mapper;
 
 import com.gestion.stock.dto.request.CommandeRequestDTO;
 import com.gestion.stock.dto.request.DetailsCommandeRequestDTO;
+import com.gestion.stock.dto.request.DetailsCommandeUpdateRequestDTO;
 import com.gestion.stock.dto.response.CommandeResponseDTO;
 import com.gestion.stock.dto.response.DetailsCommandeResponseDTO;
 import com.gestion.stock.repository.ProduitRepository;
 import com.gestion.stock.entity.DetailsCommande;
 import com.gestion.stock.entity.Produit;
 import jakarta.persistence.EntityNotFoundException;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Named;
+import org.mapstruct.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper(componentModel = "spring")
@@ -29,6 +28,12 @@ public abstract class DetailsCommandeMapper {
 
     @Mapping(target = "produitNom" , source =  "produit.nom")
     public abstract CommandeResponseDTO.DetailsCommandeResponseDTO toResponseDTO(DetailsCommande detailsCommande);
+
+
+    @Mapping(target = "id" ,ignore = true)
+    @Mapping(target = "commande", ignore = true)
+    @Mapping(target = "produit", source = "produitId", qualifiedByName = "idToProduit",nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    public abstract void updateEntity(DetailsCommandeUpdateRequestDTO updateDTO, @MappingTarget DetailsCommande detailsCommande);
 
 
     @Named("idToProduit")
