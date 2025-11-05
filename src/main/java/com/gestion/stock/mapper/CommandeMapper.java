eff6c534fd3e290943a7ac97035c2a6929219cb3
Aymane Boujeddi <aymane655@gmail.com>
2025-11-05 12:19:31 +0100

    Refactor DetailsCommande and CommandeMapper to enhance entity mapping and add update functionality for Commande

--- src/main/java/com/gestion/stock/mapper/CommandeMapper.java
+++ src/main/java/com/gestion/stock/mapper/CommandeMapper.java
@@ -2,16 +2,19 @@ package com.gestion.stock.mapper;
 
 
 import com.gestion.stock.dto.request.CommandeRequestDTO;
+import com.gestion.stock.dto.request.CommandeUpdateRequestDTO;
+import com.gestion.stock.dto.request.DetailsCommandeUpdateRequestDTO;
 import com.gestion.stock.dto.response.CommandeResponseDTO;
 import com.gestion.stock.entity.Commande;
 import com.gestion.stock.entity.Fournisseur;
 import com.gestion.stock.repository.FournisseurRepository;
 import jakarta.persistence.EntityNotFoundException;
-import org.mapstruct.AfterMapping;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Named;
+import jdk.jfr.Name;
+import org.mapstruct.*;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Collections;
+import java.util.List;
 
 @Mapper(componentModel = "spring",uses = {DetailsCommandeMapper.class})
 public abstract class  CommandeMapper {
@@ -31,10 +34,25 @@ public abstract class  CommandeMapper {
     @Mapping(target = "fournisseurNom" , source = "fournisseur.nom")
     public abstract CommandeResponseDTO toResponseDto(Commande commande);
 
+    @Mapping(target = "dateCommande" , ignore = true)
+    @Mapping(target = "montantTotale" , ignore = true)
+    @Mapping(target = "detailsCommandes" ,source = "detailsCommande" , nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_DEFAULT )
+    @Mapping(target = "statutCommande" , ignore = true )
+    @Mapping(target = "fournisseur" , source = "fournisseurId" , qualifiedByName = "idToFournisseur",nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    public abstract void updateEntity(CommandeUpdateRequestDTO updateDto, @MappingTarget Commande existingCommande);
+
 
     @Named("idToFournisseur")
     protected Fournisseur mapToFournisseur(Long fournisseurId){
         return fournisseurRepository.findById(fournisseurId).orElseThrow(() -> new EntityNotFoundException("Fournisseur not found : " + fournisseurId));
     }
+
+//    @Named("preventNullList")
+//    protected List<DetailsCommandeUpdateRequestDTO> preventNullList(List<DetailsCommandeUpdateRequestDTO> list){
+//        if(list == null){
+//            return Collections.emptyList();
+//        }
+//        return list;
+//    }
 
 }
