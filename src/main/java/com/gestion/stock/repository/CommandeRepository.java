097a81ee92b39e1488ff9bccf1400a74afc3a079
Aymane Boujeddi <aymane655@gmail.com>
2025-11-06 14:52:13 +0100

    Add @Repository annotation to CommandeRepository for proper Spring component scanning

--- src/main/java/com/gestion/stock/repository/CommandeRepository.java
+++ src/main/java/com/gestion/stock/repository/CommandeRepository.java
@@ -2,6 +2,8 @@ package com.gestion.stock.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.gestion.stock.entity.Commande;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CommandeRepository extends JpaRepository<Commande , Long> {
 }
