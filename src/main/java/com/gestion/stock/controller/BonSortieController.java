ca547e5686c76f151ddb71b50b0d00980c648b60
Aymane Boujeddi <aymane655@gmail.com>
2025-11-08 16:40:47 +0100

    SCRUM-80 Implementer endpoint Create (POST) /api/v1/bons-sortie pour Créer un bon de sortie

--- src/main/java/com/gestion/stock/controller/BonSortieController.java
+++ src/main/java/com/gestion/stock/controller/BonSortieController.java
@@ -1,11 +1,23 @@
 package com.gestion.stock.controller;
+import com.gestion.stock.dto.request.BonSortieRequestDto;
+import com.gestion.stock.dto.response.BonSortieResponseDTO;
+import com.gestion.stock.service.BonSortieService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("/api/v1/bons-sortie")
+@RequestMapping("/bonsSortie")
 @RequiredArgsConstructor
 public class BonSortieController {
+
+    private final BonSortieService bonSortieService;
+
+    @PostMapping
+    public ResponseEntity<BonSortieResponseDTO> createBonSortie(@Valid @RequestBody BonSortieRequestDto bonSortieRequestDto){
+        return ResponseEntity.ok(bonSortieService.createBonSortie(bonSortieRequestDto));
+    }
 
 }
--- src/main/java/com/gestion/stock/service/impl/BonSortieServiceImpl.java
+++ src/main/java/com/gestion/stock/service/impl/BonSortieServiceImpl.java
@@ -1,13 +1,37 @@
 package com.gestion.stock.service.impl;
 
+import com.gestion.stock.dto.request.BonSortieRequestDto;
+import com.gestion.stock.dto.response.BonSortieResponseDTO;
 import com.gestion.stock.entity.*;
+import com.gestion.stock.mapper.BonSortieItemMapper;
+import com.gestion.stock.mapper.BonSortieMapper;
+import com.gestion.stock.repository.BonSortieRepository;
 import com.gestion.stock.service.BonSortieService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 
 @Service
 @Transactional
+@RequiredArgsConstructor
 public class BonSortieServiceImpl implements BonSortieService {
 
+    private final BonSortieRepository bonSortieRepository;
+    private final BonSortieMapper mapper;
+
+
+    @Override
+    public BonSortieResponseDTO createBonSortie(BonSortieRequestDto bonSortieRequestDto) {
+        BonSortie newBonSortie = mapper.toEntity(bonSortieRequestDto);
+        newBonSortie.setDateSortie(LocalDateTime.now());
+        newBonSortie.setStatut(StatutBonSortie.BROUILLON);
+        newBonSortie.getItems().forEach(item -> item.setBonSortie(newBonSortie));
+
+        return mapper.toResponseDTO(bonSortieRepository.save(newBonSortie));
     }
+
+
+}
